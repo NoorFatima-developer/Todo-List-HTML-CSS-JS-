6729064bc15117d4457eb9d7d8d4f91b81f2cd4e
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-16 04:15:42 +0500

    code is added

---
+++ Todo List/script.js
@@ -0,0 +1,5 @@
+const inputField = document.querySelector(".input-todo")
+const addButton = document.querySelector(".btn");
+const todoList = document.querySelector("todos-list")
+
+console.log(inputField, addButton, todoList);
