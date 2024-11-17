7033c717da92b5f99b1dc05e0d57f8169e36fb1c
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-18 04:49:35 +0500

    code is added

--- Todo List/script.js
+++ Todo List/script.js
@@ -2,17 +2,27 @@ const inputField = document.querySelector("#input_box")
 const addButton = document.querySelector("#input-btn");
 const ul = document.querySelector(".ul-list")
 let userArray = [];
+let edit_id = null;
+
 
 const objStr = localStorage.getItem("users");
 if(objStr!=null){
     userArray  = JSON.parse(objStr)
 }
 
+DisplayInfo()
+
 addButton.onclick = () => {
     const name = inputField.value.trim();
+    if(name == ""){
+        alert("Please Enter a Name");
+        return;
+    }
     userArray.push({"names": name})
+    
         saveInfo(userArray)
         inputField.value = "";
+        DisplayInfo()
 }   
 
 function saveInfo(arr){
@@ -21,13 +31,26 @@ function saveInfo(arr){
 }
 
 function DisplayInfo(){
+    let statement = '';
+    userArray.forEach((user, i) => {
+        statement += `
+        <li>${user.names}
+        <button onclick="EditInfo(${i})" class = "edit">Edit</button>
+        <button onclick="DeleteInfo(${i})" class = "delete">Delete</button>
+        </li>`
+    })
 
+    ul.innerHTML = statement
 }
 
-function EditInfo(){
-
+function EditInfo(id){
+    edit_id = id;
+    inputField.value = userArray[id].names;
+    addButton.innerText = "Update"
 }
 
-function DeleteInfo(){
-
+function DeleteInfo(id){
+    userArray.splice(id, 1)
+    saveInfo(userArray);
+    DisplayInfo()
 }
