11b8fe374ffdea594cf93bba5bede2a4a7412aeb
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-17 23:28:54 +0500

    code is added

--- Todo List/script.js
+++ Todo List/script.js
@@ -1,3 +1,25 @@
-const inputFieldE = document.querySelector("#input_box")
+const inputField = document.querySelector("#input_box")
 const addButton = document.querySelector("#input-btn");
 const ul = document.querySelector(".ul-list")
+
+addButton.onclick = () => {
+    const name = inputField.value
+    
+}   
+
+
+function saveInfo(){
+    
+}
+
+function DisplayInfo(){
+
+}
+
+function EditInfo(){
+
+}
+
+function DeleteInfo(){
+
+}
